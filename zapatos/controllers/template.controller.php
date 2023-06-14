645c1bb6e1dee710b5b09d9dba8c9f05846d9858
JohanYH <johanpuerto2005@gmail.com>
2023-06-14 07:17:18 -0500

    Quiz

---
+++ zapatos/controllers/template.controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ShoesControllers{
+
+    public function shoes()
+    {
+        include "views/template.php";
+    }
+}
+
+?>
---
+++ zapatos/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("controllers/template.controller.php");
+
+$index = new ShoesControllers();
+
+$index-> shoes();
+
+?>
